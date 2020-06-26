86b97e5ed0ad4c193e68dbbe5f3819b29ca0eaa4
Vladan Denda <vladan.denda@gmail.com>
2020-06-26 14:39:16 +0200

    vino-edit

--- src/app/vina/vina-edit/vina-edit.component.ts
+++ src/app/vina/vina-edit/vina-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { VinaService } from 'src/app/vino.service';
 
 @Component({
   selector: 'app-vina-edit',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VinaEditComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  editMode = false;
+  vinaForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+    private vinaService: VinaService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.editMode = params['id'] != null;
+          this.initForm();
+        }
+      );
+  }
+  onSubmit() {
+
+    if (this.editMode) {
+      this.vinaService.updateVino(this.id, this.vinaForm.value);
+    } else {
+      this.vinaService.addVino(this.vinaForm.value);
+    }
+    this.onCancel();
   }
 
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  private initForm() {
+
+    let ime = '';
+    let vrsta = '';
+    let kolicina = '';
+    let opis = '';
+    let cena = '';
+    let imageUrl = '';
+
+
+    if (this.editMode) {
+      const vina = this.vinaService.getVino(this.id);
+
+      ime = vina.ime;
+      vrsta = vina.vrsta;
+      kolicina = vina.kolicina;
+      opis = vina.opis;
+      cena = vina.cena;
+      imageUrl = vina.imageUrl;
+
+    }
+    this.vinaForm = new FormGroup({
+      'ime': new FormControl(ime, Validators.required),
+      'vrsta': new FormControl(vrsta, Validators.required),
+      'kolicina': new FormControl(kolicina, Validators.required),
+      'opis': new FormControl(opis, Validators.required),
+      'cena': new FormControl(cena, Validators.required),
+      'imageUrl': new FormControl(imageUrl, Validators.required),
+
+    });
+  }
 }
