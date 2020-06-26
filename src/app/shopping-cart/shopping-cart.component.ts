a0bd749d1a9f780514a27c10e5de72f89c49a9a7
Vladan Denda <vladan.denda@gmail.com>
2020-06-26 14:52:24 +0200

    shopping-cart

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { VinaService } from '../vino.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Vina } from '../vino.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  vinaShoppingCart: Vina[];
+  subscription: Subscription;
+  cena:number=0;
+  @Input() index: number
+  constructor(private vinaService: VinaService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.subscription = this.vinaService.shopingCartChanged
+      .subscribe(
+        (shoppingCart: Vina[]) => {
+          this.vinaShoppingCart = shoppingCart;
+        }
+      );
+    this.vinaShoppingCart = this.vinaService.getShoppingCart();
+    
   }
 
+  cenaShoppingCart() {
+    for  (let index in this.vinaShoppingCart){
+      this.cena+= +this.vinaShoppingCart[index].cena;
+      console.log(this.cena);
+  }
+
+  }
+
+  onDeleteShoppingCartItem(index: number) {
+    this.vinaService.deleteShoppingCartItem(index);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
