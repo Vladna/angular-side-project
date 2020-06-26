f8ffd3da28b49ae5f6ceadc1f7c136c194d092fb
Vladan Denda <vladan.denda@gmail.com>
2020-06-26 14:31:51 +0200

    rute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VinaComponent } from './vina/vina.component';
+
+import { VinaEditComponent } from './vina/vina-edit/vina-edit.component';
+import { VinaDetailComponent } from './vina/vina-detail/vina-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/vina', pathMatch: 'full' },
+  {
+    path: 'vina', component: VinaComponent, children: [
+      { path: '', component: VinaComponent },
+      { path: 'new', component: VinaEditComponent },
+      { path: ':id', component: VinaDetailComponent },
+      { path: ':id/edit', component: VinaEditComponent },
+    ]
+  },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
