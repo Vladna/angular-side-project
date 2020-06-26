643dfdf4385cec32b46f12b2629cd911b7d161e1
Vladan Denda <vladan.denda@gmail.com>
2020-06-26 14:40:11 +0200

    vino-detail

--- src/app/vina/vina-detail/vina-detail.component.ts
+++ src/app/vina/vina-detail/vina-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Vina } from 'src/app/vino.model';
+import { VinaService } from 'src/app/vino.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-vina-detail',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vina-detail.component.css']
 })
 export class VinaDetailComponent implements OnInit {
+  vina: Vina;
+  id: number;
 
-  constructor() { }
+  constructor(private vinaService: VinaService,
+              private route: ActivatedRoute,
+              private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.vina = this.vinaService.getVino(this.id);
+        }
+      );
   }
 
+  onEditVina() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
+
+  onDeleteVina() {
+    this.vinaService.deleteVino(this.id);
+    this.router.navigate(['/vina']);
+  }
+  onAddVina(){
+    this.vinaService.addShoppingCart(this.id);
+    console.log('dodato vino'+this.vinaService.getVino(this.id).ime);
+    this.router.navigate(['/vina']);
+  }
 }
